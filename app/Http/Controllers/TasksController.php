57793db76f13195e9fda3652ed4865efb2cb8553
Rei Eto <etor0228@gmail.com>
2020-07-07 12:15:30 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,12 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -90,8 +95,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+            
         $task = Task::findOrFail($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,15 +9,18 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タスク</th></th>
+                    <th>タスク</th>
+                    <th>ステータス</th>
                 </tr>
             </thead>
+            
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
                     {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
                 </tr>
                 @endforeach
             </tbody>
